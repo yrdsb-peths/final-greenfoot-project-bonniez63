ea80eb947e2d68666844220defc5ef6ba5a86d80
bonniez63 <131053816+bonniez63@users.noreply.github.com>
2023-05-30 09:14:22 -0400

    Animated chicken, deleted some sprites.

--- Chicken.java
+++ Chicken.java
@@ -8,17 +8,66 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Chicken extends Actor
 {
-    public void act()
+    GreenfootImage[] idleRight = new GreenfootImage[4];
+    GreenfootImage[] idleLeft = new GreenfootImage[4];
+        
+    String facing = "right";
+    SimpleTimer animationTimer = new SimpleTimer();
+    
+    public Chicken()
     {
+        for(int i = 0; i < idleRight.length; i++)
+        {
+            idleRight[i] = new GreenfootImage("images/chicken_idle/idle" + i + ".png");
+            idleRight[i].scale(40, 70);
+        }
+            
+        for(int i = 0; i < idleLeft.length; i++)
+        {
+            idleLeft[i] = new GreenfootImage("images/chicken_idle/idle" + i + ".png");
+            idleLeft[i].scale(40, 70);
+            idleLeft[i].mirrorHorizontally();
+        }
+        
+        setImage(idleRight[0]);
+        animationTimer.mark();
+    }
+    
+    int imageIndex = 0;
+    public void animateChicken()
+    {
+        //Animates the chicken.
+        if(animationTimer.millisElapsed() < 100)
+        {
+            return;
+        }
+        animationTimer.mark();
+        
+        if(facing.equals("right"))
+        {
+            setImage(idleRight[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleRight.length;
+        }
+        else
+        {
+            setImage(idleLeft[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleLeft.length;
+        }
+    }
+    
+    public void act()
+    {    
         int x = getX();
         int y = getY();
         if(Greenfoot.isKeyDown("left"))
         {
             move(-3);
+            facing = "left";
         }
         if(Greenfoot.isKeyDown("right"))
         {
             move(3);
+            facing = "right";
         }
         if(Greenfoot.isKeyDown("up"))
         {
@@ -28,5 +77,6 @@ public class Chicken extends Actor
         {
             setLocation(x, y + 3);
         }
+        animateChicken();
     }
 }
